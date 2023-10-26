b7b5679dce0e2e507a792cadb2228d9d2bb14479
cretoxrina <141623208+cretoxrina@users.noreply.github.com>
2023-10-26 20:34:00 +0600

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,14 @@
+# text = input()
+# if text[::-1] == text:
+#     print('True')
+# else:
+#     print('False')
+
+# text = input()
+# print(text.title())
+
+# text = input()
+# print(text.replace('1','odin'))
+
+name, surname, second, position = input(), input(), input(), input()
+print(name[0]+'.', )
